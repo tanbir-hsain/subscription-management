ef0eccbeea32a759e03f93f38cacf86e2b1bcb0b
Tanbir Hussain <tanbir.hsain@gmail.com>
2026-07-04 23:08:26 +0530

    Update Subscription.java

--- src/main/java/com/tanbir/subscription/model/Subscription.java
+++ src/main/java/com/tanbir/subscription/model/Subscription.java
@@ -1,5 +1,6 @@
 package com.tanbir.subscription.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,7 +16,10 @@ public class Subscription {
     private Long id;
 
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
     private String plan;
     private String status;
 
